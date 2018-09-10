c9a2b9b5991b79df19709699477f90f1c74f7b1b
Steve Higgs <steve.higgs@salecycle.com>
2018-09-10 09:01:29 +0100

    fix entry points

--- bin/fastify-typescript-request-schema.js
+++ bin/fastify-typescript-request-schema.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 const process = require('process');
+const path = require('path');
 const convert = require('.dist/index');
-convert(process.argv[2]);
+convert(path.join(process.cwd(), process.argv[2]));
